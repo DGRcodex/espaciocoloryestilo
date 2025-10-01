00bd3f3d8acce8c89ecbc67c01fbace49286abef
Daniel García Rojas <58780631+DGRcodex@users.noreply.github.com>
2025-10-01 12:47:30 -0300

    vroma

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/vercel.webp"
+              src="/vercel.svg"
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
